21769abed7d7132c40caacf5be9b206e402f9012
Lighter888 <mishcenkomv@gmail.com>
2023-03-04 01:10:12 -0800

    add new HW

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-
-//        int a = 6;
-//        int b = 34;
-//        System.out.println(a == b);
-//        System.out.println(a < b);
-//        System.out.println(a > b);
-//
-//        if ((a + b) % 2 == 0) {
-//            System.out.println("maybe a and b are even");
-//        } else {
-//            System.out.println("some variable is odd");
-//        }
-//
-        int c = 102;
-
-        if (c > 10)  {
-            System.out.println("больше 10");
-        } else {
-            System.out.println("меньше 10");
-        }
-        if (c < 100) {
-            System.out.println("меньше 100");
-        } else {
-            System.out.println("больше 100");
-        }
-
-        if ((c / 2) > 20) {
-            System.out.println("результат деления на 2 больше 20");
-        } else {System.out.println("результат деления на 2 меньше 20");}
-        if ((c > 5) && (c <= 40)) {
-            System.out.println("значение переменной между 5 и 40 включительно");
-        } else { System.out.println("значение переменной меньше 5 или больше 40");}
-
     }
 }
-//
+
